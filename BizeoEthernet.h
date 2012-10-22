2478e3f03ef326a2b319b94e1709651e9633253c
Toby Lockley <tobylockley@gmail.com>
2012-10-22 15:19:25 +1000

    Updated to new methods, no more soap

--- BizeoEthernet.h
+++ BizeoEthernet.h
@@ -15,32 +15,21 @@
 
 #include <Ethernet.h>
 
-// Used for determining which HTTP method to use
-enum HTTP_METHOD {
-    GET,
-    POST,
-    SOAP
-};
-
 class BizeoClass {
 private:
     EthernetClient _client;
     unsigned int _debugLevel;
-    String parseXML(String searchStr);
+    int scanResponse(String searchStr);
+    String getLineFromClient();
 
 public:
     BizeoClass() { _debugLevel = 0; }
     int begin();
     int begin(uint8_t *mac_address);
     void setDebugLevel(int level);
-    // Main usage functions. When no HTTP_METHOD is specified,
-    // the default is to use GET
     int getStatus(String userGuid);
-    int getStatus(HTTP_METHOD method, String userGuid);
     int updateKpi(String kpiGuid, int value);
     int updateKpi(String kpiGuid, String value);
-    int updateKpi(HTTP_METHOD method, String kpiGuid, int value);
-    int updateKpi(HTTP_METHOD method, String kpiGuid, String value);
 };
 
 extern BizeoClass Bizeo;  // For use as global object
